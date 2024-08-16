1f616d708a99021c87d79c4407fc8f91cd9843e2
Manivannan <114678714+Manivannan0734@users.noreply.github.com>
2024-08-16 13:58:34 +0530

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -18,7 +18,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-             <Link href="https://drive.google.com/file/d/1WBhkCdbWxx9zhZ9r-jbPAOq-t_mTJTtz/view?usp=sharing">
+             <Link href="https://drive.google.com/file/d/1cZY_EkV7-ghUrLFI1yRpmeSsMVl5pw6T/view?usp=sharing">
             <Button>See My Resume</Button>
             </Link>
             <div className="mt-10">
